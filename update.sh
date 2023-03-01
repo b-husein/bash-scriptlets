34878e723abbb38b673d82713b96e7ef7fb77814
b-husein <bajrektarevic.husein@gmail.com>
2023-03-01 08:18:04 +0100

    added clear, autoremove, autoclean commands into script.

--- update.sh
+++ update.sh
@@ -16,5 +16,15 @@ sudo apt-get upgrade -y
 
 # If previous commands were executed successfully:
 echo "Your system has been updated and upgraded."
-
-
+echo "Let's clear the terminal."
+    sleep 5
+    clear
+# sleep command is a useful way to add pauses in your script.
+    sleep 2 # pause for 2 seconds.
+echo "Initializing autoremove and autoclean"
+# to remove left over dependencies of packages you no longer have.
+sudo apt autoremove -y
+# to delete all unnecessary packages from package cache.
+sudo apt autoclean -y
+    sleep 1
+echo "All done."
